3bf7f81a912ebfc183efcb6ee8f06f64a989b925
Petr Zaderey <pazaderey@edu.hse.ru>
2023-03-13 14:14:26 +0300

    🐛 Fix 12 hour

--- frontend/src/components/MainClock.jsx
+++ frontend/src/components/MainClock.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 export const MainClock = ({ clockOnly }) => {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [time, setTime] = useState(new Date().toLocaleTimeString([], { hour12: false }));
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
+      setTime(new Date().toLocaleTimeString([], { hour12: false }));
     }, 1000);
     return () => {
       clearInterval(timer);
