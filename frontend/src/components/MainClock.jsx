120267fcab4c5827dbe32eb0c70ae1648cf0b6de
Petr Zaderey <pazaderey@edu.hse.ru>
2023-03-13 23:14:19 +0300

    🐛 Fix second skipping

--- frontend/src/components/MainClock.jsx
+++ frontend/src/components/MainClock.jsx
@@ -2,22 +2,14 @@ import React, { useEffect, useState } from "react";
 
 export const MainClock = ({ clockOnly }) => {
   function getTime() {
-    return new Date().toLocaleTimeString("en-GB", {
-      hour12: false,
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    });
+    return (new Date()).toLocaleTimeString([], { hour12: false });
   }
 
   const [time, setTime] = useState(getTime());
   useEffect(() => {
-    const timer = setInterval(() => {
+    setInterval(() => {
       setTime(getTime);
     }, 1000);
-    return () => {
-      clearInterval(timer);
-    };
   });
 
   return (
